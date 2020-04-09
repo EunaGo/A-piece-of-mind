816224fdfea18b5d541608a1f48f859a8e7b0313
study_java <gumagoo8@gmail.com>
2020-04-09 18:23:33 +0900

    0409

--- Exam/src/ex1/Test2.java
+++ Exam/src/ex1/Test2.java
@@ -3,7 +3,8 @@ package ex1;
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println( "2+5" + 2+5 ); 
+		System.out.println("2+5=" + (2+5) );
 
 	}
 
