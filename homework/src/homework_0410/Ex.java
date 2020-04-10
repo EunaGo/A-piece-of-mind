6dc03d9e52ad38d600c9f289582e69f7f24a07b5
study_java <gumagoo8@gmail.com>
2020-04-10 17:29:12 +0900

    Create Ex.java

---
+++ homework/src/homework_0410/Ex.java
@@ -0,0 +1,18 @@
+package homework_0410;
+
+public class Ex {
+	
+public static void main(String[] args) {
+		
+	int a = (((25+5)+(36/4))-75)*5;
+	int b = (((25*5)+(36-4))-71)/4;
+	int c = (128/4)*2;
+		
+	if((a > b)&&(b > c)) 
+		System.out.println("true");
+	
+	else
+		System.out.println("false");
+	}
+
+}
