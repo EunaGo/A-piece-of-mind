8e592b496e081597cfae2578ff188432b028956f
Euna Go <63032830+EunaGo@users.noreply.github.com>
2020-04-10 22:46:32 +0900

    Update Ex.java

--- homework/src/homework_0410/Ex.java
+++ homework/src/homework_0410/Ex.java
@@ -4,16 +4,9 @@ public class Ex {
 	
 public static void main(String[] args) {
 		
-	int fahrenheit =100;
-	float pi = 3.141592f;
+	int num = 81;
 
-	float celcius = (int)((5.0f/9.0f * (fahrenheit - 32.0f))*100)/100f;
-	float shortpi = (int)(pi*1000)/1000f;
-	System.out.println("Fahrenheit:"+fahrenheit);
-
-	System.out.println("Celcius:" + celcius);
-	
-	System.out.println(shortpi);
+	System.out.println(10 - num % 10);
 	}
 
 }
