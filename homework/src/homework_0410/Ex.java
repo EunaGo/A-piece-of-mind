d596e0d78243f2edd216faeb7d5dc003dff1b50f
study_java <gumagoo8@gmail.com>
2020-04-13 18:12:25 +0900

    0413

--- homework/src/homework_0410/Ex.java
+++ homework/src/homework_0410/Ex.java
@@ -1,12 +1,16 @@
 package homework_0410;
 
 public class Ex {
-	
-public static void main(String[] args) {
-		
-	int num = 81;
 
-	System.out.println(10 - num % 10);
+	public static void main(String[] args) {
+
+		int fahrenheit = 100;
+
+		float celcius = ((int) ((5.0f / 9.0f * (fahrenheit - 32.0f)) * 100+0.5) / 100f);
+
+		System.out.println("Fahrenheit:" + fahrenheit);
+
+		System.out.println("Celcius:" + celcius);
+
 	}
-
 }
