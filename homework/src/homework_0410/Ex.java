6fabe5b2bc90a9ba6a95e8e5b2882aa8f9ea9328
study_java <gumagoo8@gmail.com>
2020-04-14 18:32:25 +0900

    Update Ex.java

--- homework/src/homework_0410/Ex.java
+++ homework/src/homework_0410/Ex.java
@@ -4,10 +4,13 @@ public class Ex {
 
 	public static void main(String[] args) {
 
-		int x=9;
-		
-		if(x>10&&x<20) {
-			System.out.println("treu");
+		int sum = 0;
+		for (int i = 1; i < 21; i++) {
+			if (i % 2 != 0 || i % 3 != 0) {
+				sum = sum + i;
+			}
+
 		}
+		System.out.println(sum);
 	}
 }
