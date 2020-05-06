24cd6b0355fced62c4b74ef587cd905eb80bdb51
study_java <gumagoo8@gmail.com>
2020-05-06 18:08:10 +0900

    Update Example.java

---
+++ homework/src/homework/Example.java
@@ -0,0 +1,15 @@
+package homework;
+
+import java.util.HashMap;
+
+public class Example {
+
+	public static void main(String[] args) {
+
+		String s = "  space  space  space   ";
+		s = s.trim();
+
+		System.out.println(s);
+
+	}
+}
