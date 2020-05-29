233b20ed33490e80c422954ea5e8ff13badcdbc1
study_java <gumagoo8@gmail.com>
2020-05-29 16:48:31 +0900

    0529

--- SQL/2020.05.27_view.sql
+++ SQL/2020.05.27_view.sql
@@ -36,3 +36,26 @@ order by hiredate asc
 select * from emp_hir_view;
 select rownum, empno, ename, hiredate from emp_hir_view where rownum <=5;
 select rownum, empno, ename, hiredate from emp order by hiredate;
+
+
+
+----------------------------------------------------------------
+-- sequence : 번호 자동 생성기
+----------------------------------------------------------------
+
+-- create sequence 시퀀스이름 
+-- start with 시작값  0 시작
+-- increment by 증가값 1
+-- maxvalue 최대값
+-- minvalue 최소값
+-- cycle | nocycle 반복 여부
+-- cache | nocache
+
+-- 부서번호 deptno 의 sequence 생성
+create sequence dept_deptno_seq01;
+
+select dept_deptno_seq01.nextval
+from dual;
+
+select dept_deptno_seq01.currval
+from dual;
